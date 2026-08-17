71531c5004d8c3f2f4b7606b728a59f5abf5f912
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-08-18 07:59:21 +0800

    udpate tanant delete functionality

--- src/lib/tanant/tanantdelete.js
+++ src/lib/tanant/tanantdelete.js
@@ -1,33 +1,22 @@
-"use server"
-import { headers } from "next/headers"
-import { auth } from "../auth"
+import { authToken } from "../token"
 
 const serverUrl = process.env.NEXT_PUBLIC_API_URL
 
-export const deleteData = async (id) => {
-    const token = await auth.api.getToken({
-        headers: await headers()
-    })
-    const res = await fetch(`${serverUrl}/api/my/booking/${id}`, {
+export const deleteFetchData = async (endPoint) => {
+    const token = await authToken()
+    const res = await fetch(`${serverUrl}${endPoint}`, {
         method: 'DELETE',
         headers: {
             authorization: `Bearer ${token?.token}`
         }
-
     })
     return await res.json()
 }
-export const deleteFavouriteData = async (id) => {
-    const token = await auth.api.getToken({
-        headers: await headers()
-    })
-    const res = await fetch(`${serverUrl}/api/my/favourite/${id}`, {
-        method: 'DELETE',
-        headers: {
-            authorization: `Bearer ${token?.token}`
-        }
 
-    })
-    return await res.json()
+export const deleteData = async (id) => {
+    return deleteFetchData(`/api/my/booking/${id}`)
+}
+export const deleteFavouriteData = async (id) => {
+    return deleteFetchData(`/api/my/favourite/${id}`)
 }
 
