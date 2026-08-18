978e54b6e1cd4d7f9b15d8cc4e6bd6cea817f3b3
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-08-18 08:57:33 +0800

    udpate all update function

--- src/lib/admin/updateUser.js
+++ src/lib/admin/updateUser.js
@@ -1,20 +1,22 @@
-"use server"
 
-import { headers } from "next/headers"
-import { auth } from "../auth"
+import { authToken } from "../token"
 
 const serverUri = process.env.NEXT_PUBLIC_API_URL
-export const updateUsers = async (id, data) => {
-    const { token } = await auth.api.getToken({
-        headers: await headers()
-    })
-    const res = await fetch(`${serverUri}/api/updateuser/${id}`, {
+
+export const updateFetchData = async (endPoint, data) => {
+    const token = await authToken()
+    const res = await fetch(`${serverUri}${endPoint}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
-            authorization: `Bearer ${token}`
+            authorization: `Bearer ${token?.token}`
         },
         body: JSON.stringify(data)
     })
     return await res.json()
+}
+
+
+export const updateUsers = async (id, data) => {
+    return updateFetchData(`/api/updateuser/${id}`, data)
 }
--- src/lib/owner/ownerupdate.js
+++ src/lib/owner/ownerupdate.js
@@ -1,31 +1,12 @@
-import { authToken } from "../token"
+import { updateFetchData } from "../admin/updateUser"
 
-const serverUri = process.env.NEXT_PUBLIC_API_URL
 // update status
 export const updateOwnerData = async (id, data) => {
-    const token = await authToken()
-    const res = await fetch(`${serverUri}/api/postbooking/${id}`, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${token?.token}`
-        },
-        body: JSON.stringify(data)
-    })
-    return await res.json()
+    return updateFetchData(`/api/postbooking/${id}`, data)
 }
 // update all data
 export const updateOwnerallData = async (id, data) => {
-    const token = await authToken()
-    const res = await fetch(`${serverUri}/api/updateowner/${id}`, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${token?.token}`
-        },
-        body: JSON.stringify(data)
-    })
-    return await res.json()
+    return updateFetchData(`/api/updateowner/${id}`, data)
 }
 
 
